dcd9b154842919bdf55bdd782042d53642f72d70
vuthanhtung172906 <vuthanhtung172906@gmail.com>
2021-10-16 15:44:31 +0700

    Fix baseURL

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -3,8 +3,8 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { checkTokenExp } from '../configs/checkTokenExp';
 
 const axiosClient = axios.create({
-  // baseURL: process.env.PROXY_SERVER || 'https://vtt-nodejs.herokuapp.com',
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.PROXY_SERVER || 'https://vtt-nodejs.herokuapp.com',
+  // baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
   },
